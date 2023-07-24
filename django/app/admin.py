4a2029b4011211665e5a834e59adef79a006fcd0
ShRafa <rafa.ps89@gmail.com>
2023-07-24 16:53:34 -0300

    Update

--- django/app/admin.py
+++ django/app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
+from app.models import School, Student, Subject, Grade, Teacher
+
+@admin.register(School)
+class Schooladmin(admin.ModelAdmin):
+    list_display = ["name", "city"]
+    search_fields = ["name", "city"]
+    list_filter = ["city"]
+    
+
+@admin.register(Student)
+class Studentadmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Subject)
+class Subjectadmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Grade)
+class Gradeadmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Teacher)
+class Teacheradmin(admin.ModelAdmin):
+    pass
+
+
 # Register your models here.
